4a677fa83ab7dea68c454b7e695de3f00bc3499b
Ashish Prasad <77892788+Ashish558@users.noreply.github.com>
2022-04-19 12:56:57 +0530

    Update Slider.js

--- src/pages/Design/components/Sidebar/SidebarExtension/Sliders/Slider.js
+++ src/pages/Design/components/Sidebar/SidebarExtension/Sliders/Slider.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { setActivity } from '../redux/slices/activity'
+import { setActivity } from '../../../../../../redux/slices/activity'
 
 //clicking on any theme from trending themes will open the theme in the design box
 export default function Slider({ img, name, subtype, activityType, positions }) {
